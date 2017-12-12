6b6ab2d4d8b5bd5b95306ded7228ddc33bda7550
Gibran <gibrangul@hotmail.com>
2017-12-12 23:29:05 +0500

    Add Connection listenrs

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
-
 var port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.get('/', (request, response) => {
-    response.send(publicPath + 'index.html');
-
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    
+    socket.on('disconnect', (socket) => {
+        console.log('User Disconnected');
+    });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port: ${port}`);
 });
